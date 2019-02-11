863cdc6a665d4f2e280b686fd1fb92866d5842a5
Erind Hoxha <932400893@cit3-501m-20.acg.ac.nz>
2019-02-12 09:55:20 +1300

    added handlebars

---
+++ js/data.js
@@ -0,0 +1,41 @@
+var animeList = {
+    animeListChild: [
+    {
+        img: 'img/logo1.png'
+    },
+    {
+        img: 'img/logo2.png'
+    },
+    {
+        img: 'img/logo3.png'
+    },
+    {
+        img: 'img/logo4.png'
+    },
+    {
+        img: 'img/logo5.png'
+    },
+    {
+        img: 'img/logo3.png'
+    },
+    {
+        img: 'img/logo7.png'
+    },
+    {
+        img: 'img/logo1.png'
+    },
+    {
+        img: 'img/logo2.png'
+    },
+    {
+        img: 'img/logo3.png'
+    },
+    {
+        img: 'img/logo4.png'
+    },
+    {
+        img: 'img/logo5.png'
+    },
+            
+]
+}
