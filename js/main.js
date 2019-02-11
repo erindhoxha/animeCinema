bc7cbc48463be35c5d63197f9467ec87b405f282
Erind Hoxha <932400893@cit3-501m-20.acg.ac.nz>
2019-02-12 11:16:51 +1300

    added

--- js/main.js
+++ js/main.js
@@ -4,7 +4,12 @@ xhttp.onreadystatechange = function() {
        // Typical action to be performed when the document is ready:
        var parsed = JSON.parse(xhttp.responseText)
        console.log(parsed);
+       $(".quote-generate").text(parsed.starWarsQuote);
+    } else {
+       $(".quote-generate").text('PATIENCE YOU MUST HAVE MY YOUNG PADAWAN - YODA');
     }
 };
 xhttp.open("GET", "http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote", true);
 xhttp.send();
+
+
