2588d0a45d407b644a320a483ed9c37de83174fe
Erind Hoxha <932400893@cit3-501m-20.acg.ac.nz>
2019-02-12 09:34:12 +1300

    added the animes

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       // Typical action to be performed when the document is ready:
+       var parsed = JSON.parse(xhttp.responseText)
+       console.log(parsed);
+    }
+};
+xhttp.open("GET", "http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote", true);
+xhttp.send();
