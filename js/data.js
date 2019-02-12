c6465ddd190b0a57d8bb474c99db910697457a85
Erind Hoxha <932400893@cit3-501m-20.acg.ac.nz>
2019-02-13 11:11:39 +1300

    added the overflow scroll on the main div, added the handlebars to function render

--- js/data.js
+++ js/data.js
@@ -35,42 +35,15 @@ var animeList = {
     },
     {
         img: 'img/logo5.png'
-    },
-            
+    },     
 ]
 }
 
-var animeMovieListParent = {
-    animeMovieList: [
-        {
-            img:'img/anime-list1.jpg',
-            title:'Your name',
-            date:'2002'
-        },
-        {
-            img:'img/anime-list2.jpg',
-            title:'A Silent Voice',
-            date:'2004'
-        },
-        {
-            img:'img/anime-list3.jpeg',
-            title:'Spirited Away',
-            date:'1999'
-        },
-        {
-            img:'img/anime-list4.jpeg',
-            title:'Redline',
-            date:'2013'
-        },
-        {
-            img:'img/anime-list5.jpeg',
-            title:'Howl\'s Moving Castle',
-            date:'2016'
-        },
-        {
-            img:'img/anime-list6.jpeg',
-            title:'Wolf Children',
-            date:'2018'
-        }
-    ]
-};
+
+
+var shoppingCart = {
+    shoppingCartChild: [
+        
+]
+}
+
--- js/main.js
+++ js/main.js
@@ -11,15 +11,41 @@ xhttp.onreadystatechange = function() {
 };
 xhttp.open("GET", "http://swquotesapi.digitaljedi.dk/api/SWQuote/RandomStarWarsQuote", true);
 xhttp.send();
+
 var thisIs;
 var count = 0;
+var clicked = false;
+var imageName;
+
 $(".book-wrapper").on('click', function() {
-   count++;
-   console.log(count);
-   thisIs = $(this)
-   $(this).toggleClass('animate hinge');
+    thisIs = $(this)
+    var clicked = true;
+
+    imageName = $(this).find('img').attr('src');
+
+    var imageObject = {'img': imageName};
+    shoppingCart.shoppingCartChild[shoppingCart.shoppingCartChild.length] = {...shoppingCart.shoppingCartChild[shoppingCart.shoppingCartChild.length], ...imageObject};
+    $("#shopping-cart-box").empty();
+    renderCartList();
+    if (clicked == true) {
+        $(this).toggleClass('animate hinge');
+        count++;
+        $(".shopping-cart").show(200);
+        $(".badge-shopping-cart").text(count);
+        clicked = false;
+    } else {
+        // do nothing
+    }
+
    setTimeout(function(){ 
     $(".book-wrapper").removeClass('animate hinge') 
     thisIs.css('visibility','hidden');
 }, 2000);
 })
+
+// $(".shopping-cart").hide();
+// $(".shopping-cart-box").hide();
+
+$(".shopping-cart").on('click', function() {
+    $(".shopping-cart-box").slideToggle();
+});
